ea370cc5436f950577fe3b5d812e35d13bbdf2e4
IrfanshaikH-7 <414irfann@gmail.com>
2025-01-03 23:17:22 +0530

    fix

--- src/pages/recommendations/RecommendedOnce.jsx
+++ src/pages/recommendations/RecommendedOnce.jsx
@@ -18,8 +18,8 @@ export default function RecommendedOnce() {
   }
 
   return (
-    <main className='w-full h-full py-24'>
-      <div className='max-w-7xl mx-auto gap-6 items-center '>
+    <main className='w-full h-full py-12 md:py-24'>
+      <div className='max-w-7xl px-4 mx-auto gap-6 items-center '>
         <div className=' w-40 h-40 rounded-full  mx-auto overflow-hidden shadow-xl '>
           <img src={recommendation.image} alt={recommendation.name} className='w-full h-full object-cover' />
         </div>
@@ -27,9 +27,9 @@ export default function RecommendedOnce() {
         
         <section className='flex flex-col items-center justify-center gap-3 '>
         <div className='flex flex-col items-center justify-center gap-1 '>
-        <h1 className='text-3xl font-bold'>{recommendation.name}</h1>
-            <p className='pl-1 text-neutral-600'>{recommendation.designation}</p>
-            <p className='pl-1 text-neutral-600'>{recommendation.company}</p>
+        <h1 className='text-3xl font-bold text-center' >{recommendation.name}</h1>
+            <p className='pl-1 text-neutral-600 text-sm text-center'>{recommendation.designation}</p>
+            <p className='pl-1 text-neutral-600 text-sm text-center'>{recommendation.company}</p>
         </div>
         <div className='flex flex-col gap-1 mt-1 items-center justify-center'>
         <p className='font-bold text-black text-lg text-center'>{recommendation.title}</p>
